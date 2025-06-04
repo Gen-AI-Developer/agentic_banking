fbedf48d0dd3f9bc4b5b0e0e4a3af17a15de54a3
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-04 08:50:39 +0500

    updated

--- src/agentic_banking/__init__.py
+++ src/agentic_banking/__init__.py
@@ -4,13 +4,14 @@ import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
-def main() -> None:
+def main():
+    print("Welcome to agentic-banking!")
     agent = Agent(
         name="Assistant",
-        instructions="You are a helpful assistant.",
+        instructions="You are a Master of Creating Jokes out of programming.",
         tools=[],
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key),
     )
-    result = Runner.run_sync(agent, "What is the weather in New York?")
+    result = Runner.run_sync(agent, "What is the best joke on java and C#?")
     print(result.final_output)
     print("Goodbye from agentic-banking!")
