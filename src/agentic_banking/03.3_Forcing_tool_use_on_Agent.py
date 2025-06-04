2099891f491345b7f97a7a44e0e4193b920ec516
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-04 10:41:08 +0500

    Boiler code added in every python file

---
+++ src/agentic_banking/03.3_Forcing_tool_use_on_Agent.py
@@ -0,0 +1,16 @@
+from agents import Agent, Runner, function_tool, set_tracing_disabled
+from agents.extensions.models.litellm_model import LitellmModel
+import os
+api_key = os.getenv("GEMINI_API_KEY")  
+set_tracing_disabled(disabled=True)
+
+def main():
+    print("Welcome to agentic-banking!")
+    agent = Agent(
+        name="Banking Assistant",
+        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+    )
+    result = Runner.run_sync(agent, "what is Banking?")
+    print(result.final_output)
+    print("Goodbye from agentic-banking!")
