9e1f37f2467dad678db5efcd6ee3cbddc8fb6d74
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-05 11:20:25 +0500

    working on dynamic instruction

--- src/agentic_banking/_01_2_Agents_with_Dynamic_instruction.py
+++ src/agentic_banking/_01_2_Agents_with_Dynamic_instruction.py
@@ -12,22 +12,25 @@ class UserInfo(BaseModel):
     userBalance:float
 
 def get_dynamic_instruction(context: RunContextWrapper[UserInfo], agent: Agent[UserInfo]) -> str:
-    return f"Welcome {context.user.userName}, your account type is {context.user.userAccountType} and your balance is {context.user.userBalance}. You can ask me about your account details or any banking related queries."
-    
+    return f"Welcome {context.context.userName}, your account type is {context.context.userAccountType} and your balance is {context.context.userBalance}. You can ask me about your account details or any banking related queries."
+@function_tool
+def get_user_info(context: RunContextWrapper[UserInfo], agent: Agent[UserInfo]) -> str:
+    return f"User Name: {context.context.userName}, Account Type: {context.context.userAccountType}, Balance: {context.context.userBalance}"  
 
 def main():
     userinfo = UserInfo(
         userName="Safdar Ali Shah",
         userAccount="123456789",
         userAccountType="Saving",
-        userBalance=1000.0
+        userBalance=10765490.0
     )
 
     print("Welcome to agentic-banking!")
     agent = Agent[UserInfo](
         name="Banking Assistant",
-        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        instructions=get_dynamic_instruction,
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        context=userinfo,
     )
     result = Runner.run_sync(agent, "what is Banking?")
     print(result.final_output)
