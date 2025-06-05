d5e582ce48c363c8f9bfeaca102eff977fb3e9b3
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-05 12:26:18 +0500

    dynamic instruction with handsoff done

--- src/agentic_banking/_02_Agent_handoff_with_Triage_agent.py
+++ src/agentic_banking/_02_Agent_handoff_with_Triage_agent.py
@@ -24,7 +24,23 @@ class UserInfo(BaseModel):
     userAccountCurrency: str = "PKR"  # Default currency, can be changed if needed
 
 def get_dynamic_instruction(context: RunContextWrapper[UserInfo], agent: Agent[UserInfo]) -> str:
-    return f"Welcome {context.context.userName}, Customer / User account No. {context.context.userAccountNo}, Customer / User account type is {context.context.userAccountType}, Customer / User Account balance is {context.context.userAccountBalance}, and Customer / User account currency is {context.context.userAccountCurrency}. You can ask me about your account details or any banking related queries."
+    return f"""
+    ALWAYS START WITH YOUR NAME THAT (THAT I'M Intreset Finding AGENT)
+        You are a helpfull assistant, who help customer in 
+        finding Annual Per Return on Investment (APRI) which is  Interest on Savings. 
+        You will be provided with the user's account balance and interest rate.
+        Calculate the interest on the user's savings based on the provided interest rate.
+        The interest rate will be provided in percentage.
+        Use the formula: Interest = (Balance * Interest Rate) / 100
+        Respond with the calculated interest amount in the same currency as the user's account balance.
+        For example, if the user's account balance is in PKR, respond with the interest amount in PKR.
+        
+    You use These Detailed provided according to customer need and use, 
+    which is Customer / User name :  {context.context.userName}, 
+    Customer / User account No. {context.context.userAccountNo}, 
+    Customer / User account type is {context.context.userAccountType}, 
+    Customer / User Account balance is {context.context.userAccountBalance}, 
+    and Customer / User account currency is {context.context.userAccountCurrency}."""
 
 def main():
     print("Welcome to Triage Agent!")
@@ -44,25 +60,15 @@ def main():
     )
     InterestFinderAgent = Agent(
         name="Banking Interest Finder Assistant",
-        instructions="""
-        ALWAYS START WITH YOUR NAME THAT (THAT I'M Intreset Finding AGENT)
-        You are a helpfull assistant, who help customer in 
-        finding Annual Per Return on Investment (APRI) which is  Interest on Savings. 
-        You will be provided with the user's account balance and interest rate.
-        Calculate the interest on the user's savings based on the provided interest rate.
-        The interest rate will be provided in percentage.
-        Use the formula: Interest = (Balance * Interest Rate) / 100
-        Respond with the calculated interest amount in the same currency as the user's account balance.
-        For example, if the user's account balance is in PKR, respond with the interest amount in PKR.""",
+        instructions=get_dynamic_instruction,
         model=MODEL,
     )
     TriageAgent = Agent[UserInfo](
         name="Triage Agent",
-        instructions=get_dynamic_instruction,
+        instructions="Your are a Triage Agent, who triage the customer request and handoff to the appropriate agent based on the user's needs.",
         model=MODEL,
         handoffs=[CustomerServiceAgent, InterestFinderAgent],
     )
     result = Runner.run_sync(TriageAgent, "I have a question about my bank account, my question is: Calculate the APRI on my savings in my account, if the Interest rate is 3.2 percent?",context=userinfo)
-    print("Triage Agent Result:")
     print(result.final_output)
     print("Goodbye from agentic-banking!")
