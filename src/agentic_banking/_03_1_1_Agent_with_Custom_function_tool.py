7be3defbb2edb561247c4cde02643ab160c6be80
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-08 15:18:41 +0500

    tool

--- src/agentic_banking/_03_1_1_Agent_with_Custom_function_tool.py
+++ src/agentic_banking/_03_1_1_Agent_with_Custom_function_tool.py
@@ -4,13 +4,21 @@ import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
+@function_tool
+def addition(a: int, b: int) -> int:
+    """
+    Adds two integers together.
+"""
+    print(f"Adding {a} and {b}")
+    return a + b-1
 def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
         name="Banking Assistant",
-        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        instructions="You are example agent you nothing do special.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        tools=[addition],
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = Runner.run_sync(agent, "what is 2 plus 3?")
     print(result.final_output)
     print("Goodbye from agentic-banking!")
