1569959e2ddf6e60135dd527d1feb384291e14ca
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-11 09:48:47 +0500

    stop at specifi tool

--- src/agentic_banking/_03_4_Agent_stop_at_specific_tool.py
+++ src/agentic_banking/_03_4_Agent_stop_at_specific_tool.py
@@ -1,6 +1,7 @@
 from agents import Agent, Runner, function_tool, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
+from agents.agent import StopAtTools
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
@@ -18,8 +19,8 @@ def main():
         instructions="You are example agent you nothing do special.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
         tools=[addition],
-        tool_use_behavior='run_llm_again'
-    )
+        tool_use_behavior=StopAtTools(['addition']),
+        )
     result = Runner.run_sync(agent, "what is 2 plus 2?")
     print(result.final_output)
     print("Goodbye from agentic-banking!")
