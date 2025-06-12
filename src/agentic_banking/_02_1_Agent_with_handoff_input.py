035c819bde3dfce1f6a46e06f0d33a48f0027f65
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-12 23:06:58 +0500

    update

--- src/agentic_banking/_02_1_Agent_with_handoff_input.py
+++ src/agentic_banking/_02_1_Agent_with_handoff_input.py
@@ -1,9 +1,26 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled
+from agents import Agent, Runner, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
+from agents import Agent, handoff, RunContextWrapper
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
+from pydantic import BaseModel
 
+
+
+class EscalationData(BaseModel):
+    reason: str
+
+async def on_handoff(ctx: RunContextWrapper[None], input_data: EscalationData):
+    print(f"Escalation agent called with reason: {input_data.reason}")
+
+agent = Agent(name="Escalation agent")
+
+handoff_obj = handoff(
+    agent=agent,
+    on_handoff=on_handoff,
+    input_type=EscalationData,
+)
 def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
