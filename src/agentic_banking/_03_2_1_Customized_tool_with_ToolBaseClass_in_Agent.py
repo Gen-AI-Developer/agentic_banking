209bf895227d59d9ba8c96eb6435e7d9c07f4b17
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-12 12:04:44 +0500

    src/agentic_banking/_03_2_1_Customized_tool_with_ToolBaseClass_in_Agent.py

--- src/agentic_banking/_03_2_1_Customized_tool_with_ToolBaseClass_in_Agent.py
+++ src/agentic_banking/_03_2_1_Customized_tool_with_ToolBaseClass_in_Agent.py
@@ -1,5 +1,5 @@
 from typing import Any
-from agents import Agent, RunContextWrapper, Runner, function_tool, set_tracing_disabled
+from agents import Agent, FunctionTool, RunContextWrapper, Runner, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 from pydantic import BaseModel
@@ -11,20 +11,29 @@ class FunctionArguments(BaseModel):
     age: int
     email: str
 def do_some_work(data: str) -> str:
-    return {"Status":"done", "data": data}
+    print(f"Processing do_some_work function")
+    return f"Processed data: {data}"
 
 async def run_function(context: RunContextWrapper[Any],args:str)-> str:
+    print(f"Running function with args")
     parsed = FunctionArguments.model_validate_json(args)
     return do_some_work(f"Username: {parsed.name}, Age: {parsed.age}, Email: {parsed.email}")
-    
 
+tool = FunctionTool(
+    name="User info Processor",
+    description="Process user information from the input string.",
+    params_json_schema=FunctionArguments.model_json_schema(),
+    on_invoke_tool=run_function,
+    strict_json_schema=True,
+)   
 def main():
-    print("Welcome to agentic-banking!")
+    print("Welcome to Assistant!")
     agent = Agent(
-        name="Banking Assistant",
-        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        name="Assistant",
+        instructions="You are a helpfull assistant",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        tools=[tool],
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = Runner.run_sync(agent, "process this user info: name John Doe, age 30, email programmersafdar@live.com")
     print(result.final_output)
-    print("Goodbye from agentic-banking!")
+    print("Goodbye from Assistant!")
