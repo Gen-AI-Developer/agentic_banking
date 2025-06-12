bdc82cc8073931662539703b93d74feedc3d15b4
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-12 12:39:38 +0500

    ailure_error_function

--- src/agentic_banking/_03_3_1_Agents_with_tools_Errors.py
+++ src/agentic_banking/_03_3_1_Agents_with_tools_Errors.py
@@ -4,13 +4,19 @@ import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
+@function_tool(name_override="do_some_work_tool",failure_error_function=None)
+def do_some_work(data: str) -> str:
+    print(f"do_some_work function with data: {data}")
+    print("Processing data...")
+    return f"Processed data: {data}"
+
 def main():
-    print("Welcome to agentic-banking!")
+    print("Welcome to Assistant!")
     agent = Agent(
-        name="Banking Assistant",
-        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        name="Assistant",
+        instructions="You are a helpfull assistant",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
     result = Runner.run_sync(agent, "what is Banking?")
     print(result.final_output)
-    print("Goodbye from agentic-banking!")
+    print("Goodbye from Assistant!")
