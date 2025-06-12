cb28b75bcd7febece2f1cfe82125386041a029e4
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-12 08:37:14 +0500

    src/agentic_banking/_03_2_1_Customized_tool_with_ToolBaseClass_in_Agent.py

--- src/agentic_banking/_03_2_1_Customized_tool_with_ToolBaseClass_in_Agent.py
+++ src/agentic_banking/_03_2_1_Customized_tool_with_ToolBaseClass_in_Agent.py
@@ -1,8 +1,22 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled
+from typing import Any
+from agents import Agent, RunContextWrapper, Runner, function_tool, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
+from pydantic import BaseModel
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
+
+class FunctionArguments(BaseModel):
+    name: str
+    age: int
+    email: str
+def do_some_work(data: str) -> str:
+    return {"Status":"done", "data": data}
+
+async def run_function(context: RunContextWrapper[Any],args:str)-> str:
+    parsed = FunctionArguments.model_validate_json(args)
+    return do_some_work(f"Username: {parsed.name}, Age: {parsed.age}, Email: {parsed.email}")
+    
 
 def main():
     print("Welcome to agentic-banking!")
--- src/agentic_banking/_03_3_Forcing_tool_use_on_Agent.py
+++ src/agentic_banking/_03_3_Forcing_tool_use_on_Agent.py
@@ -1,4 +1,4 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled
+from agents import Agent, ModelSettings, Runner, function_tool, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
@@ -10,7 +10,10 @@ def main():
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        model_settings=ModelSettings(
+           tool_choice="required",  # This forces the agent to use tools
+        ),
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = Runner.run_sync(agent, "what is Banking?",)
     print(result.final_output)
     print("Goodbye from agentic-banking!")
