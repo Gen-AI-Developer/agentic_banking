c095c9c2dd29b808dd1443b16a7721956885b589
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-19 12:52:15 +0500

    working on runhooks

--- src/agentic_banking/_10_3_Agent_with_Lifecycle_agent_run_hooks.py
+++ src/agentic_banking/_10_3_Agent_with_Lifecycle_agent_run_hooks.py
@@ -6,11 +6,18 @@ set_tracing_disabled(disabled=True)
 
 def main():
     print("Welcome to AI Assistant with Lifecycle (Agent-Run) Hooks!")
-    agent = Agent(
-        name="Assistant",
-        instructions="You are a helpfull assistant, answer in a concise way.",
+    open_ai_expert_agent = Agent(
+        name="Expert_OpenAI_Agents_SDK",
+        instructions="You are a helpfull Open AI Agents SDK Expert Assistant, answer in a concise way.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        handoff_description="This agent is specialized in OpenAI Agents SDK related questions. If the question is not related to OpenAI Agents SDK, it will handoff to the Triage Agent.",
     )
-    result = Runner.run_sync(agent, "what is agent?")
+    Triage_agent = Agent(
+        name="Triage Agent",
+        instructions="You are a helpfull Triage Agent, you will triage the question and pass it to the appropriate agent.",
+        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        handoff=[open_ai_expert_agent]
+    )
+    result = Runner.run_sync(Triage_agent, "what is agent as per Open AI?")
     print(result.final_output)
     print("Goodbye from AI Assistant with Lifecycle (Agent-Run) Hooks!")
