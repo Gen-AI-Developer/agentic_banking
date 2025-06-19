e1ae32409bab1f5667edaf7ee619b8cb1bf6a78b
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-19 12:32:20 +0500

    updated

--- src/agentic_banking/_02_1_Agent_with_handoff_input.py
+++ src/agentic_banking/_02_1_Agent_with_handoff_input.py
@@ -27,6 +27,7 @@ def main():
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        handoffs=[handoff_obj],
     )
     result = Runner.run_sync(agent, "what is Banking?")
     print(result.final_output)
