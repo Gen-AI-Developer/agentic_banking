593c7c95ff7917a9a2e6520b9f12a0a3aad58b39
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-22 00:31:53 +0500

    simpel agents with runconfig and modelsetting

--- src/agentic_banking/_01_Agents.py
+++ src/agentic_banking/_01_Agents.py
@@ -1,9 +1,17 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled
+from agents import Agent, Runner, function_tool, set_tracing_disabled, RunConfig, ModelSettings
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
+modelsetting = ModelSettings(
+    max_tokens=100,
+    temperature=0.5,
+    top_p=0.9,
+)
+myconfig = RunConfig(
+    model_settings=modelsetting,
+)
 def main():
     print("Welcome to agentic-banking!")
     """
@@ -15,6 +23,6 @@ def main():
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = Runner.run_sync(agent, "Banking?", max_turns=1,run_config=myconfig)
     print(result.final_output)
     print("Goodbye from agentic-banking!")
