428b3a3177902daccdd74b8aac6bc18e4a0a0183
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-22 13:05:57 +0500

    stream updated

---
+++ src/agentic_banking/_14_Agent_Result_to_input_list.py
@@ -0,0 +1,25 @@
+from agents import Agent, Runner, function_tool, set_tracing_disabled
+from openai.types.responses import ResponseTextDeltaEvent
+from agents.extensions.models.litellm_model import LitellmModel
+import os
+import asyncio
+api_key = os.getenv("GEMINI_API_KEY")  
+set_tracing_disabled(disabled=True)
+
+async def main():
+    print("Welcome to AI")
+    agent = Agent(
+        name="Assistant",
+        instructions="You are a helpfull assistant",
+        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+    )
+    result = Runner.run_streamed(agent, "what is today date?")
+    async for event in result.stream_events:
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            print(event.raw_response, end="", flush=True)
+        pass
+
+    print("Goodbye from ai!")
+
+if __name__ == "__main__":
+    asyncio.run(main())
