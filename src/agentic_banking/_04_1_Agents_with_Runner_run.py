afc962b49b11667dccb59983ec8a07d52e96a621
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-22 22:25:08 +0500

    update

--- src/agentic_banking/_04_1_Agents_with_Runner_run.py
+++ src/agentic_banking/_04_1_Agents_with_Runner_run.py
@@ -3,14 +3,17 @@ from agents.extensions.models.litellm_model import LitellmModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
-
-def main():
+async def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = await Runner.run(agent, "what is Banking?")
     print(result.final_output)
     print("Goodbye from agentic-banking!")
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
