6a0998939539e25f0a9a8cef4fd346836406ff54
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-23 09:55:19 +0500

    updated

--- src/agentic_banking/_13_Agent_tracing.py
+++ src/agentic_banking/_13_Agent_tracing.py
@@ -5,12 +5,12 @@ api_key = os.getenv("GEMINI_API_KEY")
 set_tracing_disabled(disabled=True)
 
 def main():
-    print("Welcome to agentic-banking!")
+    print("Welcome to AI Assistant!")
     agent = Agent(
         name="Banking Assistant",
-        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        instructions="You are a helpfull assistant",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
     result = Runner.run_sync(agent, "what is Banking?")
     print(result.final_output)
-    print("Goodbye from agentic-banking!")
+    print("Welcome to AI Assistant!")
