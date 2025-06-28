369e6f62795efc9319f8de7acb80a990c1df013e
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-28 23:44:18 +0500

    Structure output with tools and agents

--- src/agentic_banking/_09_1_Agents_with_Tool_Structure_output.py
+++ src/agentic_banking/_09_1_Agents_with_Tool_Structure_output.py
@@ -9,13 +9,23 @@ class StructureOutput(BaseModel):
     capital: str
     country: str
 
+@function_tool
+def _countryCapitals(input: str) -> StructureOutput:
+    """
+    This tool returns the capital and country for a given input.
+    """
+    return StructureOutput(
+        capital="Paris",
+        country="France"
+    )
 def main():
     print("Welcome to AI Assistant")
     agent = Agent(
         name="AI Assistant",
         instructions="You are a helpfull assistant",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
-        output_type=StructureOutput,  # Specify the output type
+        output_type=StructureOutput,
+        tools=[_countryCapitals]  # Specify the output type
     )
     result = Runner.run_sync(agent, "what is the capital of France and what country is it in?")
     print(result.final_output)
