bae8828ca797aa377f0f44e4365c81527a8a25e8
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-28 23:50:35 +0500

    working with llm context

--- src/agentic_banking/_06_Agents_with_llm_Context.py
+++ src/agentic_banking/_06_Agents_with_llm_Context.py
@@ -4,13 +4,15 @@ import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
+context=""" Explain Banking for a 5 year old. in 3 Lines"""
+
 def main():
-    print("Welcome to agentic-banking!")
+    print("Welcome to Assistant with Context")
     agent = Agent(
-        name="Banking Assistant",
+        name="Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = Runner.run_sync(agent, "what is Banking?",context=context, )
     print(result.final_output)
-    print("Goodbye from agentic-banking!")
+    print("Goodbye from Assistant with context!")
