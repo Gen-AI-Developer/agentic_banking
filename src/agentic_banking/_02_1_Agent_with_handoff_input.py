efcbbdd9b66c2c110e7fc31fcf9e2af831dd42c5
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-29 12:11:40 +0500

    updated

--- src/agentic_banking/_02_1_Agent_with_handoff_input.py
+++ src/agentic_banking/_02_1_Agent_with_handoff_input.py
@@ -2,11 +2,13 @@ from agents import Agent, Runner, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 from agents import Agent, handoff, RunContextWrapper
-api_key = os.getenv("GEMINI_API_KEY")  
+gemini_api_key = os.getenv("GEMINI_API_KEY")  
+if gemini_api_key:
+   print("key is provided")# Debugging line to check if the key is loaded
+else:
+    print("GEMINI_API_KEY is not set. Please set it in your environment variables.")
 set_tracing_disabled(disabled=True)
 from pydantic import BaseModel
-
-
 
 class EscalationData(BaseModel):
     reason: str
@@ -26,9 +28,9 @@ def main():
     agent = Agent(
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
-        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=gemini_api_key ,),
         handoffs=[handoff_obj],
     )
-    result = Runner.run_sync(agent, "what is Banking?")
+    result = Runner.run_sync(agent, "I was charged twice for a transaction; please resolve this issue.")
     print(result.final_output)
     print("Goodbye from agentic-banking!")
