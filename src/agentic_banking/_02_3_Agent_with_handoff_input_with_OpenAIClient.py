fedc9c9d5fe939086a8721f7ead2eb97af4ee4ea
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-29 12:31:05 +0500

    updated

--- src/agentic_banking/_02_3_Agent_with_handoff_input_with_OpenAIClient.py
+++ src/agentic_banking/_02_3_Agent_with_handoff_input_with_OpenAIClient.py
@@ -1,15 +1,38 @@
 from agents import Agent, Runner, set_tracing_disabled
-from agents.extensions.models.litellm_model import LitellmModel
 import os
 from agents import Agent, handoff, RunContextWrapper
 from pydantic import BaseModel
+from agents import (
+    AsyncOpenAI,
+    OpenAIChatCompletionsModel,
+    RunConfig
+)
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")  
+
 if gemini_api_key:
    print("key is provided")# Debugging line to check if the key is loaded
 else:
     print("GEMINI_API_KEY is not set. Please set it in your environment variables.")
-set_tracing_disabled(disabled=True)
+
+
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
 
 class EscalationData(BaseModel):
     reason: str
@@ -29,9 +52,8 @@ def main():
     agent = Agent(
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking. if you incounter any issue/problems/escalation, you can handoff to escalation agent.",
-        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=gemini_api_key),
         handoffs=[handoff_obj],
     )
-    result = Runner.run_sync(agent, "I was charged twice for a transaction; please resolve this issue.")
+    result = Runner.run_sync(agent, "I was charged twice for a transaction; please resolve this issue.",run_config=config)
     print(result.final_output)
     print("Goodbye from agentic-banking!")
