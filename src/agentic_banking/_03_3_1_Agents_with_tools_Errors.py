9b9915b7e95c3afcb29ddda5c965e2599aefcfd0
SyedSafdarAliShah <programmersafdar@live.com>
2025-06-30 12:40:08 +0500

    updated

--- src/agentic_banking/_03_3_1_Agents_with_tools_Errors.py
+++ src/agentic_banking/_03_3_1_Agents_with_tools_Errors.py
@@ -1,14 +1,14 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled, ModelSettings
+from agents import Agent, RunContextWrapper, Runner, function_tool, set_tracing_disabled, ModelSettings
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
-def handle_error(error: Exception) -> str:
-    print(f"An error occurred: {error}")
-    return "An error occurred while processing your request. Please try again later."
+def handle_error(context: RunContextWrapper, error: Exception) -> str:
+    print(f"An error occurred: {error} with context: {context}")
+    return "An error occurred while processing the tool your request. Please try again later."
 
-@function_tool(name_override="do_some_work_tool",failure_error_function=handle_error)
+@function_tool(name_override="do_some_work_tool",failure_error_function=handle_error,)
 def do_some_work(data: str) -> str:
     print(f"do_some_work function with data: {data}")
     print("Processing data...")
@@ -26,12 +26,11 @@ def main():
         instructions="You are a helpfull Banking only assistant",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
         tools=[do_some_work],
-        max_iterations=3,
         model_settings=ModelSettings(
-            tool_choice="do_some_work"
+            tool_choice="do_some_work_tool",
         ),
-        tool_use_behavior="stop_on_first_tool"
+        tool_use_behavior="run_llm_again"
     )
-    result = Runner.run_sync(agent, "what is Physics?")
+    result = Runner.run_sync(agent, "no data")
     print(result.final_output)
     print("Goodbye from Assistant!")
