d285072e3873037697c8027742d3b26ca5ae00df
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-03 12:18:21 +0500

    exploring streaming

--- src/agentic_banking/_03_Agent_with_Tool.py
+++ src/agentic_banking/_03_Agent_with_Tool.py
@@ -1,4 +1,4 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled
+from agents import Agent, Runner, function_tool, set_tracing_disabled,enable_verbose_stdout_logging
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 # from dataclasses import asdict
@@ -6,7 +6,7 @@ from agentic_banking.printt import printt
 from agentic_banking.printt import pprint
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
-
+enable_verbose_stdout_logging()
 @function_tool
 def addition(a: int, b: int) -> int:
     """
@@ -17,14 +17,14 @@ def addition(a: int, b: int) -> int:
 def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
-        name="Banking Assistant",
-        instructions="You are example agent you nothing do special.",
+        name=True,
+        instructions=12345678920,
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
         tools=[addition],
         tool_use_behavior='run_llm_again'
     )
     result = Runner.run_sync(agent, "what is 2 plus 2?")
     # print(result.final_output)
-    printt(result)
+    # printt(result)
     pprint(result)
     print("Goodbye from agentic-banking!")
--- src/agentic_banking/_04_3_Agents_with_Runner_streaming.py
+++ src/agentic_banking/_04_3_Agents_with_Runner_streaming.py
@@ -1,16 +1,31 @@
+import asyncio
 from agents import Agent, Runner, function_tool, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
-def main():
+async def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
-    result = Runner.run_sync(agent, "what is Banking?")
-    print(result.final_output)
+    result = Runner.run_streamed(agent, "what is Banking?")
+    async for event in result.stream_events():
+        print(f"\n Event {event.type}: {event}")
+        if event.type == "raw_response_event":
+            pass
+            # print(f"\n Event name:{event.type} ==  {event.name}")
+            # print(f"\n Event type:{event.type} ==   {event.type}")
+            # print(f"\n Event data:{event.type} ==   {event.data}")
     print("Goodbye from agentic-banking!")
+
+if __name__ == "__main__":
+    # Run the main function using asyncio
+    # This is necessary to run async functions in Python
+    # It allows us to use the event loop to run our async code
+    # This is a common pattern in Python for running async code
+    # It is necessary to run async functions in Python   
+    asyncio.run(main())
