0e7aef9f191e9c1f2b7413073bf3140f2ced3154
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-04 21:29:07 +0500

    updated

--- src/agentic_banking/_03_Agent_with_Tool.py
+++ src/agentic_banking/_03_Agent_with_Tool.py
@@ -1,3 +1,4 @@
+from dataclasses import asdict
 from agents import Agent, Runner, function_tool, set_tracing_disabled,enable_verbose_stdout_logging
 from agents.extensions.models.litellm_model import LitellmModel
 import os
@@ -7,7 +8,7 @@ from agentic_banking.printt import printt
 from agentic_banking.printt import pprint
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
-enable_verbose_stdout_logging()
+# enable_verbose_stdout_logging()
 
 @function_tool
 def get_weather(city: str) -> str:
@@ -31,16 +32,31 @@ def addition(a: int, b: int) -> int:
     print(f"Adding {a} and {b}")
     return a + b - 5
 def main():
+    print("==========================================================")
+    
     print("Welcome to agentic-banking!")
+    print("==========================================================")
+
     agent = Agent(
-        name=main.__name__,
-        instructions=main(),
+        name=True | False,
+        instructions=False,
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
         tools=[addition,],
         tool_use_behavior=StopAtTools(Stop_at_tools_names=["get_weather"]),
     )
+    print(f"Type of Agent is {type(agent)}: with values : {asdict(agent)}")
+    print("==========================================================")
+
+    print(f"Type of Agent Name : {type(agent.name)}")
+    print("==========================================================")
+
+    print(f"Type of Agent Instructions : {type(agent.instructions)}")
+    print("==========================================================")
+
+
     result = Runner.run_sync(agent, "what is 2 plus 2? ane what is the weather in New York?")
     # print(result.final_output)
     # printt(result)
-    pprint(result)
+    print("==========================================================")
+    # print(result.final_output)
     print("Goodbye from agentic-banking!")
