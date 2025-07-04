381eda309a3df70445de9fbd9a257abfce2369b8
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-04 05:56:17 +0500

    streamingouput

--- src/agentic_banking/_04_3_Agents_with_Runner_streaming.py
+++ src/agentic_banking/_04_3_Agents_with_Runner_streaming.py
@@ -1,16 +1,31 @@
 import asyncio
+from dataclasses import asdict
 from agents import Agent, Runner, function_tool, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
+
+from agentic_banking import printt
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
+
+
+#This Tool will be used to called/proved UserInfoExtractor function
+@function_tool
+async def UserInfoExtractor(username :str, userpassword: str) -> str:
+    """Extracts user information from a query.
+    Args:        query (str): The query from which to extract user information.
+    Returns:        str: A string containing the extracted user information.
+    """
+
+    return f"Extracted user information from query: {username} and {userpassword} successfully." 
 async def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        tools=[UserInfoExtractor],
     )
     result = Runner.run_streamed(agent, "what is Banking?")
     async for event in result.stream_events():
@@ -20,6 +35,7 @@ async def main():
             # print(f"\n Event name:{event.type} ==  {event.name}")
             # print(f"\n Event type:{event.type} ==   {event.type}")
             # print(f"\n Event data:{event.type} ==   {event.data}")
+    printt(asdict(result))
     print("Goodbye from agentic-banking!")
 
 if __name__ == "__main__":
