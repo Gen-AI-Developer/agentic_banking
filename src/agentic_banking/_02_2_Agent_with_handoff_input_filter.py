2a11d27e7e9e457831eb7b851a9d3c1f189c7c82
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-09 23:28:44 +0500

    setting mdoel

--- src/agentic_banking/_02_2_Agent_with_handoff_input_filter.py
+++ src/agentic_banking/_02_2_Agent_with_handoff_input_filter.py
@@ -3,12 +3,13 @@ from agents.extensions.models.litellm_model import LitellmModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
+MODEL=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key)
 
 def main():
     print("Welcome to agentic-banking!")
     triage_agent = Agent(
         name="Triage Assistant",
-        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        
     )
     result = Runner.run_sync(triage_agent, "what is Banking?")
     print(result.final_output)
