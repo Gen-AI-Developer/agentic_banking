bea7a3186bf43972af32d5427fbb3d0028b6d7af
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-11 09:28:31 +0500

    Input Guardrails

--- src/agentic_banking/_07_1_Agents_with_input_Guardrails.py
+++ src/agentic_banking/_07_1_Agents_with_input_Guardrails.py
@@ -1,4 +1,5 @@
-from agents import Agent, GuardrailFunctionOutput, InputGuardrailTripwireTriggered, RunContextWrapper, Runner, function_tool, input_guardrail, set_tracing_disabled
+import asyncio
+from agents import Agent, GuardrailFunctionOutput, InputGuardrailTripwireTriggered, RunContextWrapper, Runner, TResponseInputItem, function_tool, input_guardrail, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 from pydantic import BaseModel
@@ -7,8 +8,8 @@ api_key = os.getenv("GEMINI_API_KEY")
 set_tracing_disabled(disabled=True)
 
 class BankingQuestion(BaseModel):
-    is_banking_question: bool = False
-    reason: str = "The question / Query is not related to banking."
+    is_banking_question: bool
+    reason: str 
 
 guardrail_agent = Agent(
     name="Guardrail Agent",
@@ -18,28 +19,31 @@ guardrail_agent = Agent(
 )
 
 @input_guardrail
-def math_guardrails(context: RunContextWrapper[None], agent: Agent,input: str) -> GuardrailFunctionOutput:
+async def math_guardrails(context: RunContextWrapper[None], agent: Agent,input: str | list[TResponseInputItem]) -> GuardrailFunctionOutput:
     """
     Guardrail function to check if the input text contains question / query / operation related to Banking.
     If it doesn't, it returns a message indicating that other than banking operations are not allowed.
     """
-    guardrail_agent_response = Runner.run_sync(guardrail_agent, input)
+    guardrail_agent_response = await Runner.run(guardrail_agent, input,context=context.context)
     return GuardrailFunctionOutput(
             output_info=guardrail_agent_response.final_output,
             tripwire_triggered=guardrail_agent_response.final_output.reason,
         )
 
-def main():
+async def main():
     print("Welcome to agentic-banking!")
     agent = Agent(
         name="Banking Assistant",
         instructions="You are a helpfull assistant, who help in customer service and banking.",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
-        input_guardrails=math_guardrails,
+        input_guardrails=[math_guardrails],
     )
     try :
-        result = Runner.run_sync(agent, "what is Banking?")
+        result = await Runner.run(agent, "what is Banking?")
         print(result.final_output)
     except InputGuardrailTripwireTriggered as e:
-        print(f"Guardrail Triggered Tripwire Triggered: {e}")
+        print(f"Guardrail Tripwire Triggered: {e}")
     print("Goodbye from agentic-banking!")
+
+if __name__ == "__main__":
+    asyncio.run(main())
