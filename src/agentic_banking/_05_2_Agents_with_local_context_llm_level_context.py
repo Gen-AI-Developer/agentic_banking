e44d7167c1d153f80965189eb868f41e8b8bb5e6
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-19 19:45:21 +0500

    gitUpdate

--- src/agentic_banking/_05_2_Agents_with_local_context_llm_level_context.py
+++ src/agentic_banking/_05_2_Agents_with_local_context_llm_level_context.py
@@ -1,16 +1,51 @@
-from agents import Agent, Runner, function_tool, set_tracing_disabled
+from agents import Agent, RunContextWrapper, Runner, function_tool, set_tracing_disabled, AgentHooks, RunHooks
 from agents.extensions.models.litellm_model import LitellmModel
+from pydantic import BaseModel
 import os
 api_key = os.getenv("GEMINI_API_KEY")  
 set_tracing_disabled(disabled=True)
 
+class UserInfo(BaseModel):
+    name: str
+    age: int
+
+class CustomeRunHooks(RunHooks):
+    def on_agent_start(self, context:RunContextWrapper[UserInfo]):
+        print(f"RunHOok : Run started: {context.context.name}")
+
+    def on_agent_end(self, run):
+        print(f"Run ended: {run.id}")
+
+class CustomeAgentHooks(AgentHooks):
+    def on_rstat(self,context: RunContextWrapper[UserInfo], agent):
+        print(f"AgentHOoK: Agent started: {agent.name} + {context.context.name}")
+
+    def on_end(self, agent):
+        print(f"Agent ended: {agent.name}")
+
+
+
+
 def main():
+    userinfor = UserInfo(name="John Doe", age=30)
     print("Welcome to agentic-banking!")
-    agent = Agent(
+    agent = Agent[UserInfo](
         name="Banking Assistant",
-        instructions="You are a helpfull assistant, who help in customer service and banking.",
+        instructions="You are a helpfull assistant",
+        model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
+        hooks=CustomeAgentHooks(),
+    )
+    result = Runner.run_sync(agent, "what is Bthe name of the user?", context=userinfor, )
+
+
+
+    print(f"Result of First Agent: {result.final_output}")
+
+    agent2 = Agent(
+        name="Banking Assistant",
+        instructions="You are a helpfull assistant",
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
     )
-    result = Runner.run_sync(agent, "what is Banking?")
-    print(result.final_output)
+    result2 = Runner.run_sync(agent2, "what is the name of the user?", context=userinfor, hooks=CustomeRunHooks())
+    print(result2.final_output)
     print("Goodbye from agentic-banking!")
