20ec88e596603bd998edb5583a429cd7e2ca6c2a
SyedSafdarAliShah <programmersafdar@live.com>
2025-07-20 22:28:50 +0500

    Updage

--- src/agentic_banking/_01_2_Agents_with_Dynamic_instruction.py
+++ src/agentic_banking/_01_2_Agents_with_Dynamic_instruction.py
@@ -30,14 +30,14 @@ def main():
     # print(userinfo)
     # dynamicInstruction = f"Your are expert at Banking Operation, this is user data he may ask any question {userinfo}"
     print("Welcome to agentic-banking!")
-    agent = Agent(
+    agent = Agent[UserInfo](
         name="Banking Assistant",
-        instructions="Your Are Assistant",
+        instructions=get_dynamic_instruction,
         # tool_use_behavior='stop_on_first_tool',
         model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key,),
 
     )
-    result = Runner.run_sync(agent, f"what is my bank account Number and balance?{userinfo}", max_turns=3  )
+    result = Runner.run_sync(agent, f"what is my bank account Number and balance?", context=userinfo max_turns=3  )
     print(result.final_output)
     print("----------------------------------------------")
     # printt.printt(result)
